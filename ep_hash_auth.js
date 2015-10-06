36f4a7de367c9dac87e6192ee209c14813b63dbf
Robin Schneider <ypid@riseup.net>
2015-10-06 23:57:20 +0200

    Use bcrypt instead of SHA2-512. The SHA2 in this setup is insecure.
    SHA2 with one round for storing passwords is anything but secure these
    days. Bcrypt seems to be the better option.
    
    Related: https://github.com/debops/ansible-etherpad/issues/14

--- ep_hash_auth.js
+++ ep_hash_auth.js
@@ -1,32 +1,28 @@
-// hash based authentication for etherpad
-// 2014 - István Király - LaKing@D250.hu
+/* hash based authentication for etherpad
+ * 2014 - István Király - LaKing@D250.hu
+ * Reworked by Robin Schneider <ypid@riseup.net> to allow the use of proper password hash algorithms.
+ */
 
 var settings = require('ep_etherpad-lite/node/utils/Settings');
 var authorManager = require('ep_etherpad-lite/node/db/AuthorManager');
-var crypto = require('crypto');
-
-var hash_typ = "sha512";
-var hash_dig = "hex";
-
-if (settings.ep_hash_auth) {
-    if (settings.ep_hash_auth.hash_typ) hash_typ = settings.ep_hash_auth.hash_typ;
-    if (settings.ep_hash_auth.hash_dig) hash_dig = settings.ep_hash_auth.hash_dig;
-}
+var bcrypt = require('bcrypt');
 
 exports.authenticate = function(hook_name, context, cb) {
-  console.debug('ep_hash_auth.authenticate');
+  // console.debug('ep_hash_auth.authenticate');
+  // console.log(context.req.headers.authorization);
 
   if (context.req.headers.authorization && context.req.headers.authorization.search('Basic ') === 0) {
     var userpass = new Buffer(context.req.headers.authorization.split(' ')[1], 'base64').toString().split(":")
-    var username = userpass.shift();
+      var username = userpass.shift();
     var password = userpass.join(':');
-    var hash = crypto.createHash(hash_typ).update(password).digest(hash_dig);
+    // console.log(username);
+    // console.log(password);
 
     // Authenticate user via settings.json
     if (settings.users[username] != undefined) {
       // hash defined in "hash" of users
       if (settings.users[username].hash != undefined) {
-        if (settings.users[username].hash == hash) {
+        if (bcrypt.compareSync(password, settings.users[username].hash)) {
           settings.users[username].username = username;
           context.req.session.user = settings.users[username];
           return cb([true]);
