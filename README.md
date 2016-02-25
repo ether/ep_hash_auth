a1b3cfe80d49393a9b2c9277876ea8b9e9f6d455
LaKing <LaKing@D250.hu>
2016-02-25 07:10:33 +0100

    v2.x

--- README.md
+++ README.md
@@ -1,14 +1,43 @@
-This plugin allows the usage of hash values for authentoication in settings.json
+# ep_hash_auth
 
+This [etherpad-lite] plugin allows the usage of hashed passwords for authentication.
+As of version 2.x it uses the crypto lib and/or the bcrypt lib [bcrypt](https://www.npmjs.com/package/bcrypt) for comparison.
+Besides settings.json, it is now possible to store the user-database in a filesystem hierarchy. The hash files are read on authentication.
+
+```JSON
   "users": {
 	"admin": {"password": "admin","is_admin": true},
 	"y": {"is_admin": true, "hash": "b2112aa73994bd7cbb07732326ee102d585e706bb2e4fb878df5b5706ea92522f67b9f9dbc0208e2bd5b0f9cb21221bfb970f36a63e27e1a128ebc44d1ea5976"}
   }
+```JSON
 
-optionally specify hash type and digest, defaults are:
+optionally specify hash type and digest, folders and extension, defaults are:
 
+```JSON
   "ep_hash_auth": {
     "hash_typ": "sha512",
-    "hash_dig": "hex"
+    "hash_dig": "hex",
+    "hash_dir": "/var/etherpad/users",
+    "hash_ext": "/.hash",
+    "hash_adm": false
   },
-  
+```
+This means user Alice would have to have her hash in sha512 hex OR in bcrypt format in the following file:
+```Shell
+/var/etherpad/users/Alice/.hash
+```
+The hash_adm parameter defines the role of file-authenticated users, by default they are not admins.
+
+## Generate the hashes
+```Shell
+ 	 apt-get install -yqq python-bcrypt
+ 	 python -c 'from passlib.hash import bcrypt;print(bcrypt.encrypt("password", rounds=10))'
+```
+
+## Credits
+
+* István Király [LaKing](https://github.com/LaKing) (author and maintainer)
+* [Robin `ypid` Schneider](https://github.com/ypid) (contributor)
+
+## the npm
+(https://www.npmjs.com/package/ep_hash_auth)
--- ep_hash_auth.js
+++ ep_hash_auth.js
@@ -1,6 +1,108 @@
+// 2.x hash based authentication for etherpad
+// 2014-2016 - István Király - LaKing@D250.hu
+// Contributions by Robin Schneider <ypid@riseup.net>
+
+// Made on codepad :P
+
+var fs = require('fs');
+var settings = require('ep_etherpad-lite/node/utils/Settings');
+var authorManager = require('ep_etherpad-lite/node/db/AuthorManager');
+var sessionManager = require('ep_etherpad-lite/node/db/SessionManager');
+var crypto = require('crypto');
+
+// npm install bcrypt
+var bcrypt = require('bcrypt');
+
+// ocrypt-relevant options
+var hash_typ = "sha512";
+var hash_dig = "hex";
+
+// default dir to search for hash files
+var hash_dir = '/var/etherpad/users';
+// by default the extension is actually a file, so usernames are actually folders
+var hash_ext = '/.hash';
+// by default peple logged in that authenticated over a hash file, are admins?
+var hash_adm = false;
+
+
+if (settings.ep_hash_auth) {
+    if (settings.ep_hash_auth.hash_typ) hash_typ = settings.ep_hash_auth.hash_typ;
+    if (settings.ep_hash_auth.hash_dig) hash_dig = settings.ep_hash_auth.hash_dig;
+    if (settings.ep_hash_auth.hash_dir) hash_dir = settings.ep_hash_auth.hash_dir;
+    if (settings.ep_hash_auth.hash_ext) hash_ext = settings.ep_hash_auth.hash_ext;
+    if (settings.ep_hash_auth.hash_adm) hash_adm = settings.ep_hash_auth.hash_adm;
+}
+
+exports.authenticate = function(hook_name, context, cb) {
+    if (context.req.headers.authorization && context.req.headers.authorization.search('Basic ') === 0) {
+        var userpass = new Buffer(context.req.headers.authorization.split(' ')[1], 'base64').toString().split(":");
+        var username = userpass.shift();
+        var password = userpass.join(':');
+
+        var hash = crypto.createHash(hash_typ).update(password).digest(hash_dig);
+
+        // Authenticate user via settings.json
+        if (settings.users[username] !== undefined) {
+            // hash defined in "hash" of users
+            if (settings.users[username].hash !== undefined) {
+                if (settings.users[username].hash == hash) {
+                    console.log("Authenticated (crypto) " + username);
+                    settings.users[username].username = username;
+                    context.req.session.user = settings.users[username];
+                    return cb([true]);
+                } else {
+                    bcrypt.compare(password, settings.users[username].hash, function(err, res) {
+                        if (err || !res) return cb([false]);
+                        else {
+                            console.log("Authenticated (bcrypt) " + username);
+                            settings.users[username].username = username;
+                            context.req.session.user = settings.users[username];
+                            return cb([true]);
+                        }
+                    });
+                }
+            } else return cb([false]);
+
+        } else {
+            // Authenticate user via hash_dir
+            var path = hash_dir + "/" + username + hash_ext;
+            fs.readFile(path, 'utf8', function(err, contents) {
+                if (err) {
+                    // file not found, or inaccessible
+                    console.log("AUTH: cannot authenticate " + username);
+                    return cb([false]);
+                } else {
+                    if (contents === hash) {
+                        console.log("Authenticated (crypto-file) " + username);
+                        settings.users[username] = {};
+                        settings.users[username].username = username;
+                        settings.users[username].is_admin = hash_adm;
+                        context.req.session.user = settings.users[username];
+                        return cb([true]);
+                    } else {
+                        bcrypt.compare(password, contents, function(err, res) {
+                            if (err || !res) return cb([false]);
+                            else {
+                                console.log("Authenticated (bcrypt-file) " + username);
+                                settings.users[username] = {};
+                                settings.users[username].username = username;
+                                settings.users[username].is_admin = hash_adm;
+                                context.req.session.user = settings.users[username];
+                                return cb([true]);
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    } else return cb([false]);
+
+};
+
+// 1.0.2 version archived
 // hash based authentication for etherpad
 // 2014 - István Király - LaKing@D250.hu
-
+/*
 var settings = require('ep_etherpad-lite/node/utils/Settings');
 var authorManager = require('ep_etherpad-lite/node/db/AuthorManager');
 var crypto = require('crypto');
@@ -36,3 +138,4 @@ exports.authenticate = function(hook_name, context, cb) {
   }
   return cb([false]);
 };
+*/
