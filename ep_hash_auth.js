d8e63ade52f00c7917308341674d53945e542d10
LaKing <LaKing@D250.hu>
2016-04-19 00:30:14 +0200

    bugix

--- ep_hash_auth.js
+++ ep_hash_auth.js
@@ -42,9 +42,7 @@ exports.authenticate = function(hook_name, context, cb) {
         var hash = crypto.createHash(hash_typ).update(password).digest(hash_dig);
 
         // Authenticate user via settings.json
-        if (settings.users[username] !== undefined) {
-            // hash defined in "hash" of users
-            if (settings.users[username].hash !== undefined) {
+        if (settings.users[username] !== undefined && settings.users[username].hash !== undefined) {
                 if (settings.users[username].hash == hash) {
                     console.log("Authenticated (crypto) " + username);
                     settings.users[username].username = username;
@@ -61,8 +59,6 @@ exports.authenticate = function(hook_name, context, cb) {
                         }
                     });
                 }
-            } else return cb([false]);
-
         } else {
             // Authenticate user via hash_dir
             var path = hash_dir + "/" + username + hash_ext;
@@ -98,44 +94,3 @@ exports.authenticate = function(hook_name, context, cb) {
     } else return cb([false]);
 
 };
-
-// 1.0.2 version archived
-// hash based authentication for etherpad
-// 2014 - István Király - LaKing@D250.hu
-/*
-var settings = require('ep_etherpad-lite/node/utils/Settings');
-var authorManager = require('ep_etherpad-lite/node/db/AuthorManager');
-var crypto = require('crypto');
-
-var hash_typ = "sha512";
-var hash_dig = "hex";
-
-if (settings.ep_hash_auth) {
-    if (settings.ep_hash_auth.hash_typ) hash_typ = settings.ep_hash_auth.hash_typ;
-    if (settings.ep_hash_auth.hash_dig) hash_dig = settings.ep_hash_auth.hash_dig;
-}
-
-exports.authenticate = function(hook_name, context, cb) {
-  console.debug('ep_hash_auth.authenticate');
-
-  if (context.req.headers.authorization && context.req.headers.authorization.search('Basic ') === 0) {
-    var userpass = new Buffer(context.req.headers.authorization.split(' ')[1], 'base64').toString().split(":")
-    var username = userpass.shift();
-    var password = userpass.join(':');
-    var hash = crypto.createHash(hash_typ).update(password).digest(hash_dig);
-
-    // Authenticate user via settings.json
-    if (settings.users[username] != undefined) {
-      // hash defined in "hash" of users
-      if (settings.users[username].hash != undefined) {
-        if (settings.users[username].hash == hash) {
-          settings.users[username].username = username;
-          context.req.session.user = settings.users[username];
-          return cb([true]);
-        }
-      }
-    }
-  }
-  return cb([false]);
-};
-*/
